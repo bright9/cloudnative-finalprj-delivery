688a6cec4ea0c7a2a04eb0c128d61de4916d227a
이명구 <myonggu.lee@kt.com>
2024-04-04 13:57:53 +0900

    1

---
+++ src/main/java/finalproject/domain/Delivery.java
@@ -0,0 +1,57 @@
+package finalproject.domain;
+
+import finalproject.DeliveryApplication;
+import finalproject.domain.DeliveryStarted;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Delivery_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String address;
+
+    private String customerId;
+
+    private Integer qty;
+
+    private Long orderId;
+
+    @PostPersist
+    public void onPostPersist() {
+        DeliveryStarted deliveryStarted = new DeliveryStarted(this);
+        deliveryStarted.publishAfterCommit();
+    }
+
+    public static DeliveryRepository repository() {
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(
+            DeliveryRepository.class
+        );
+        return deliveryRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void addToDeliveryList(OrderPlaced orderPlaced) {
+        //implement business logic here:
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(orderPlaced.getAddress());
+        delivery.setQuantity(orderPlaced.getQty());
+        delivery.setCustomerId(orderPlaced.getCustomerId());
+        delivery.setOrderId(orderPlaced.getId());
+        repository().save(delivery);
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
