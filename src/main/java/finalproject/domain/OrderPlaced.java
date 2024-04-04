0a8aee58b8aa87b3ca5c164bbe6394e147687436
이명구 <myonggu.lee@kt.com>
2024-04-04 14:50:14 +0900

    1

--- src/main/java/finalproject/domain/OrderPlaced.java
+++ src/main/java/finalproject/domain/OrderPlaced.java
@@ -13,7 +13,6 @@ public class OrderPlaced extends AbstractEvent {
     private String productId;
     private Integer qty;
     private String customerId;
-    private Double amount;
     private String status;
     private String address;
 }
